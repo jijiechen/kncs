470d3f8a229ff2ed797770882318bf70a62b6664
Jijie Chen <jijie.chen@someplace.com>
2022-05-02 17:46:11 +0800

    支持从 CSharp 代码中动态地生成 Pod

--- Kncs.CmdExecuter/Program.cs
+++ Kncs.CmdExecuter/Program.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using System.Dynamic;
 using System.Reflection;
+using k8s;
+using k8s.Models;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
@@ -9,7 +11,7 @@ namespace Kncs.CmdExecuter;
 
 public class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         Console.WriteLine("已安装的 kubectl 版本: ");
         Console.WriteLine(ExecuteKubectl("version", null));
@@ -18,6 +20,11 @@ public class Program
         var podName = $"testpod-{random}";
 
         var podYaml = GetPodYaml(podName);
+        if (args.Length > 0)
+        {
+            podName = $"csharp-{podName}";
+            podYaml = ComposePodAsYaml(podName);
+        }
         Console.WriteLine($"准备创建 Pod {podName}：");
         Console.WriteLine(podYaml);
         
@@ -65,6 +72,32 @@ public class Program
         serializer.Serialize(writer, p);
 
         return writer.ToString();
+    }
+    
+    
+    static string ComposePodAsYaml(string podName)
+    {
+        var pod = new V1Pod
+        {
+            ApiVersion = "v1",
+            Kind = "Pod",
+            Spec = new V1PodSpec(){Containers = new List<V1Container>()},
+            Metadata = new V1ObjectMeta(){Labels = new Dictionary<string, string>()}
+        };
+
+        pod.Metadata.Name = podName;
+        pod.Metadata.Labels["app"] = "test";
+        
+        var container = new V1Container("tester")
+        {
+            Args = new[] { "infinity" },
+            Command = new[] { "sleep" },
+            Image = "centos:7",
+            ImagePullPolicy = "IfNotPresent"
+        };
+        pod.Spec.Containers.Add(container);
+
+        return KubernetesYaml.Serialize(pod);
     }
 
 }
